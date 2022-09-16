291acd45005656c660e1796d8beeed15bec4a934
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-09-16 15:24:25 +0100

    Initial Commit

---
+++ Colour/ColourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourApp.swift
+//  Colour
+//
+//  Created by Jack Finnis on 16/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
